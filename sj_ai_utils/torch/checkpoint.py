296a1c45d00c5592fdc2a8d23aac3565ef939b0d
SangJung <sangjung245@gmail.com>
2025-07-12 08:47:43 +0000

    [FEAT] 로드 함수 수정

--- sj_ai_utils/torch/checkpoint.py
+++ sj_ai_utils/torch/checkpoint.py
@@ -1,8 +1,15 @@
 import torch
-import time
 
 from dataclasses import dataclass
 from pathlib import Path
+
+DEFAULT_SAVE_NAME = "checkpoint"
+EPOCH = "epoch"
+MODEL_KEY = "model_state_dict"
+OPTIMIZER_KEY = "optimizer_state_dict"
+SCHEDULER_KEY = "scheduler_state_dict"
+LOSS_KEY = "loss"
+TORCH_DEFAULT_DEVICE = torch.device("cpu")
 
 
 @dataclass(slots=True)
@@ -20,17 +27,61 @@ class Checkpoint:
             self.best = loss
             self.save()
 
-    def save(self, name:str = "checkpoint"):
+    def save(self, name: str = DEFAULT_SAVE_NAME):
         torch.save(
             {
-                "epoch": self.epoch,
-                "model_state_dict": self.model.state_dict(),
-                "optimizer_state_dict": self.optimizer.state_dict(),
-                "scheduler_state_dict": self.scheduler.state_dict(),
-                "loss": self.best,
+                EPOCH: self.epoch,
+                MODEL_KEY: self.model.state_dict(),
+                OPTIMIZER_KEY: self.optimizer.state_dict(),
+                SCHEDULER_KEY: self.scheduler.state_dict(),
+                LOSS_KEY: self.best,
             },
             self.path / f"{name}.pt",
         )
+
+    @staticmethod
+    def __load(
+        target: str,
+        model: torch.nn.Module,
+        path: Path = None,
+        loaded_data: dict = None,
+        device: torch.device = TORCH_DEFAULT_DEVICE,
+    ):
+        if loaded_data is None and path is not None:
+            model.load_state_dict(torch.load(path, map_location=device)[target])
+            return model
+        elif loaded_data is not None:
+            model.load_state_dict(loaded_data[target])
+            return model
+        else:
+            raise ValueError("Either path or loaded_data must be provided.")
+
+    @staticmethod
+    def load_model(
+        model: torch.nn.Module,
+        path: Path = None,
+        loaded_data: dict = None,
+        device: torch.device = TORCH_DEFAULT_DEVICE,
+    ):
+        return Checkpoint.__load(MODEL_KEY, model, path, loaded_data, device)
+
+    @staticmethod
+    def load_optimizer(
+        model: torch.nn.Module,
+        path: Path = None,
+        loaded_data: dict = None,
+        device: torch.device = TORCH_DEFAULT_DEVICE,
+    ):
+        return Checkpoint.__load(OPTIMIZER_KEY, model, path, loaded_data, device)
+
+    @staticmethod
+    def load_scheduler(
+        model: torch.nn.Module,
+        path: Path = None,
+        loaded_data: dict = None,
+        device: torch.device = TORCH_DEFAULT_DEVICE,
+    ):
+        return Checkpoint.__load(SCHEDULER_KEY, model, path, loaded_data, device)
 
     @staticmethod
     def load(
@@ -42,9 +93,9 @@ class Checkpoint:
         device: torch.device = torch.device("cpu"),
     ):
         ckpt = torch.load(pt_path, map_location=device)
-        model.load_state_dict(ckpt["model_state_dict"])
-        optimizer.load_state_dict(ckpt["optimizer_state_dict"])
-        scheduler.load_state_dict(ckpt["scheduler_state_dict"])
+        model = Checkpoint.load_model(model, path, ckpt, device)
+        optimizer = Checkpoint.load_optimizer(optimizer, path, ckpt, device)
+        scheduler = Checkpoint.load_scheduler(scheduler, path, ckpt, device)
         return Checkpoint(
             path=path,
             model=model,

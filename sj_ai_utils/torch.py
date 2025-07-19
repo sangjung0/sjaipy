bc4665013580809e34bc9d2ee82d6a5de9855260
SangJung <sangjung245@gmail.com>
2025-07-19 19:12:43 +0000

    [FEAT] 토치 유틸 추가

---
+++ sj_ai_utils/torch.py
@@ -0,0 +1,18 @@
+import torch
+import base64
+import io
+
+
+def tensor_to_base64(tensor: torch.Tensor) -> str:
+    if tensor is None:
+        return ""
+    buffer = io.BytesIO()
+    torch.save(tensor, buffer)
+    return base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+
+def base64_to_tensor(encoded_str: str) -> torch.Tensor | None:
+    if not encoded_str:
+        return None
+    buffer = io.BytesIO(base64.b64decode(encoded_str.encode("utf-8")))
+    return torch.load(buffer)

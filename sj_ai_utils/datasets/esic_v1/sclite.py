aa23bdab43c75d7ec8b85ff3786a162bfd4c60a0
SangJung <sangjung245@gmail.com>
2025-08-11 14:04:45 +0000

    [FEAT] sclite 평가 모듈에 전용 데이터 로더 추가

--- sj_ai_utils/datasets/esic_v1/sclite.py
+++ sj_ai_utils/datasets/esic_v1/sclite.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
-from pathlib import Path
-from typing import Callable
+import numpy as np
 
+from pathlib import Path
+from typing import Callable, Generator
+from functools import lru_cache
+
+from sj_utils.audio import load_audio_from_mp4
 from sj_ai_utils.evaluator.sclite_utils import TRNFormat
 from sj_ai_utils.datasets.esic_v1.service import select_file_from_dir
 from sj_ai_utils.datasets.esic_v1.file_type import MP4, VERBATIM, ORTO
@@ -12,42 +16,51 @@ if TYPE_CHECKING:
     pass
 
 
-def generate_trn(
+@lru_cache(maxsize=4196)
+def load_mp4_to_audio(mp4, sr):
+    return load_audio_from_mp4(mp4, sr=sr)
+
+
+def load_data(
+    data_paths: list[Path], sr: int
+) -> Generator[tuple[np.ndarray, str, str, Path], None, None]:
+    for path in data_paths:
+        key = str(path.absolute())[:255]
+        mp4 = select_file_from_dir(path, MP4)
+        audio = load_mp4_to_audio(mp4, sr)[0]
+        txt = select_file_from_dir(path, ORTO)
+        txt = txt.read_text(encoding="utf-8")
+
+        yield audio, key, txt, path
+
+
+def generate_ref_and_hyp(
     data_paths: list[Path],
-    transcribe: Callable[[Path], str],
+    transcriber: Callable[[np.ndarray, Path], str],
     normalizer: Callable[[str], str] = lambda x: x,
     max_count: int = -1,
-) -> dict[str, dict[str, list[TRNFormat]]]:
-    if not data_paths:
-        raise ValueError("No data paths provided.")
-    if not all(p.exists() for p in data_paths):
-        raise ValueError("One or more data paths do not exist.")
+    sr: int = 16_000,
+) -> tuple[list[TRNFormat], list[TRNFormat]]:
+    if any(not p.exists() or not p.is_dir() for p in data_paths):
+        raise FileNotFoundError("One or more data paths do not exist.")
 
-    result = {}
-    for i, data_path in enumerate(data_paths):
+    result_ref = []
+    result_hyp = []
+    for i, (audio, key, y, path) in enumerate(load_data(data_paths, sr=sr)):
         if max_count != -1 and i >= max_count:
             break
 
-        key = data_path.absolute()
-
-        src = select_file_from_dir(data_path, ORTO)
-        txt = src.read_text(encoding="utf-8")
-        txt = normalizer(txt)
+        txt = normalizer(y)
         ref = TRNFormat(id=key, text=txt)
 
-        mp4 = select_file_from_dir(data_path, MP4)
-        pred_txt = transcribe(mp4)
-        pred_txt = normalizer(pred_txt)
-        hyp = TRNFormat(id=key, text=pred_txt)
+        pred = transcriber(audio, path)
+        pred = normalizer(pred)
+        hyp = TRNFormat(id=key, text=pred)
 
-        result[key] = {
-            "ref": [ref],
-            "hyp": [hyp],
-        }
+        result_ref.append(ref)
+        result_hyp.append(hyp)
 
-    return result
+    return result_ref, result_hyp
 
 
-__all__ = [
-    "generate_trn",
-]
+__all__ = ["generate_ref_and_hyp", "load_data"]
--- sj_ai_utils/datasets/libri_speech_asr_corpus/sclite.py
+++ sj_ai_utils/datasets/libri_speech_asr_corpus/sclite.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+import librosa
+import numpy as np
+
 from pathlib import Path
-from typing import Callable
+from typing import Callable, Generator
+from functools import lru_cache
 
 from sj_ai_utils.evaluator.sclite_utils import TRNFormat
 from sj_ai_utils.datasets.libri_speech_asr_corpus.file_type import X, Y
@@ -12,46 +16,69 @@ if TYPE_CHECKING:
     pass
 
 
-def trans_txt_to_sclite_trn(
-    src: Path, normalizer: Callable[[str], str] = lambda x: x
-) -> list[TRNFormat]:
+@lru_cache(maxsize=4196)
+def load_flac(flac: Path, sr: int) -> tuple[np.ndarray, int]:
+    return librosa.load(flac, sr=sr)
+
+
+def trans_txt_to_key_data(trans: Path) -> list[tuple[str, Path]]:
     result = []
-    with src.open("r", encoding="utf-8") as fin:
+    with trans.open("r", encoding="utf-8") as fin:
         for line in fin:
             if not line.strip():
                 continue
             uid, *words = line.rstrip().split()
-            sent = normalizer(" ".join(words))
-            result.append(TRNFormat(id=uid, text=sent))
+            sent = " ".join(words)
+            result.append((uid, sent))
     return result
 
 
-def generate_trn(
+def load_data(
+    data_paths: list[Path], sr: int
+) -> Generator[tuple[np.ndarray, str, str, Path], None, None]:
+    for path in data_paths:
+        if not path.exists():
+            raise FileNotFoundError(f"Data path {path} does not exist.")
+        if not path.is_dir():
+            raise ValueError(f"Data path {path} should be a directory.")
+
+        key_data = trans_txt_to_key_data(path)
+        for key, data in key_data:
+            flac = path / f"{key}.flac"
+            if not flac.exists():
+                raise FileNotFoundError(f"FLAC file {flac} does not exist.")
+
+            audio, _ = load_flac(flac, sr)
+            yield audio, key, data, flac
+
+
+def generate_ref_and_hyp(
     data_paths: list[Path],
-    transcribe: Callable[[Path], str],
+    transcriber: Callable[[np.ndarray, Path], str],
     normalizer: Callable[[str], str] = lambda x: x,
     max_count: int = -1,
+    sr: int = 16_000,
 ) -> dict[str, dict[str, list[TRNFormat]]]:
-    if not data_paths:
-        raise ValueError("No data paths provided.")
-    if not all(p.exists() for p in data_paths):
+    if any(not p.exists() for p in data_paths):
         raise ValueError("One or more data paths do not exist.")
 
-    result = {}
-    for i, dirpath in enumerate(data_paths):
+    result_ref = []
+    result_hyp = []
+    for i, (audio, key, y, path) in enumerate(load_data(data_paths, sr=sr)):
         if max_count != -1 and i >= max_count:
             break
 
-        trans_txt = select_file_from_dir(dirpath, Y)
-        ref_items = trans_txt_to_sclite_trn(trans_txt, normalizer=normalizer)
-        hyp_items = [
-            TRNFormat(id=flac.stem, text=normalizer(transcribe(flac)))
-            for flac in sorted(select_file_from_dir(dirpath, X))
-        ]
+        txt = normalizer(y)
+        ref = TRNFormat(id=key, text=txt)
 
-        result[trans_txt.stem] = {"ref": ref_items, "hyp": hyp_items}
+        pred = transcriber(audio, path)
+        pred = normalizer(pred)
+        hyp = TRNFormat(id=key, text=pred)
 
-    return result
+        result_ref.append(ref)
+        result_hyp.append(hyp)
+
+    return result_ref, result_hyp
 
 
-__all__ = ["trans_txt_to_sclite_trn", "generate_trn"]
+__all__ = ["generate_ref_and_hyp"]
